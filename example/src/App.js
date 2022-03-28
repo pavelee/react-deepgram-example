19cd160a9f16ef629790fb2c170c7be91d2cb628
Paweł Ciosek <ciosek.pawel@gmail.com>
2022-03-28 22:48:14 +0200

    deepgram

--- example/src/App.js
+++ example/src/App.js
@@ -1,9 +1,59 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 
 const DeepgramHandler = (props) => {
-    return <>{props.children}</>;
+    const [mediaRecorder, setMediaRecorder] = useState(null);
+
+    const startRecord = () => {
+        navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+            const mr = new MediaRecorder(stream);
+            setMediaRecorder(mr);
+
+            mr.start();
+
+            const audioChunks = [];
+
+            mr.addEventListener("dataavailable", (event) => {
+                audioChunks.push(event.data);
+            });
+
+            mr.addEventListener("stop", () => {
+                const audioBlob = new Blob(audioChunks);
+                console.log(audioChunks);
+                const audioUrl = URL.createObjectURL(audioBlob);
+                const audio = new Audio(audioUrl);
+                console.log(audio);
+                audio.play();
+            });
+
+            //   setTimeout(() => {
+            //     mr.stop();
+            //   }, 3000);
+        });
+    };
+
+    const stopRecord = (mediaRecorder) => {
+        mediaRecorder.stop();
+        setMediaRecorder(null);
+    };
+
+    return (
+        <>
+            <button
+                onClick={() => {
+                    startRecord();
+                }}
+            >
+                transcript
+            </button>
+            {mediaRecorder && (
+                <button onClick={() => stopRecord(mediaRecorder)}>stop!</button>
+            )}
+
+            {props.children}
+        </>
+    );
 };
 
 function App() {
@@ -15,7 +65,6 @@ function App() {
                     <DeepgramHandler>
                         <input style={{ padding: 15 }} />
                     </DeepgramHandler>
-                    <button>transcript!</button>
                 </p>
             </header>
         </div>
