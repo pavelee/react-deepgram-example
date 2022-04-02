e1b674ea98d494672819da0cd543bcef6470b694
Paweł Ciosek <ciosek.pawel@gmail.com>
2022-04-02 23:02:48 +0200

    deepgram

--- deepgram-proxy/server.js
+++ deepgram-proxy/server.js
@@ -2,6 +2,41 @@ const express = require("express");
 var cors = require("cors");
 var app = express();
 
+const deepgramApiKey = "x";
+
+const deepgramTranscript = async (deepgramApiKey, filepath, filemime = 'audio/wav') => {
+    const deepgram = new Deepgram(deepgramApiKey);
+    const mimetype = filemime;
+    const file = filepath;
+    const audio = fs.readFileSync(file);
+
+    // Set the source
+    source = {
+        buffer: audio,
+        mimetype: mimetype,
+    };
+
+    let finalResponse = {}
+
+    // Send the audio to Deepgram and get the response
+    await deepgram.transcription
+        .preRecorded(source, {
+            punctuate: true,
+        })
+        .then((response) => {
+            // Write the response to the console
+//            finalResponse = response
+            finalResponse = response.results.channels[0].alternatives[0].transcript
+            // Write only the transcript to the console
+            //console.dir(response.results.channels[0].alternatives[0].transcript, { depth: null });
+        })
+        .catch((err) => {
+            finalResponse = err
+        });
+
+    return finalResponse;
+}
+
 const multer = require("multer");
 
 const storage = multer.diskStorage({
@@ -29,7 +64,24 @@ app.get("/", (req, res) => {
 app.post("/audio", upload.single("file"), (req, res) => {
     console.log(req.body);
     console.log(req.file);
-    res.send({ message: "Successfully uploaded: " + req.file.filename });
+    let filepath = req.file.path
+    res.send({ message: "Successfully uploaded: " + req.file.path });
+});
+
+app.post("/audiotranscript", upload.single("file"), async (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+    let filepath = req.file.path
+    let transcript = await deepgramTranscript(deepgramApiKey, filepath);
+    res.send({ transcript: transcript });
+});
+
+const fs = require("fs");
+const { Deepgram } = require("@deepgram/sdk");
+
+app.get("/deepgram", async (req, res) => {
+    let transcript = await deepgramTranscript(deepgramApiKey, 'uploads/1311.wav');
+    res.json(transcript);
 });
 
 app.listen(PORT, HOST);
--- example/src/App.js
+++ example/src/App.js
@@ -1,25 +1,42 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const DeepgramHandler = (props) => {
+    const { setValue } = props;
     const [mediaRecorder, setMediaRecorder] = useState(null);
+    const [transcript, setTranscript] = useState(null);
+    const [fetchingTranscript, setFetchingTranscript] = useState(false);
+
+    useEffect(() => {
+        if (setValue && transcript) {
+            setValue(transcript);
+        }
+    }, [transcript]);
 
     const uploadFile = (file) => {
         console.log("Uploading file...", file);
-        const API_ENDPOINT = "http://localhost:8080/audio";
+        const API_ENDPOINT = "http://localhost:8080/audiotranscript";
 
-        const formData = new FormData()
-        formData.append('username', 'Sandra Rodgers')
+        const formData = new FormData();
+        formData.append("username", "Sandra Rodgers");
         // formData.append('file', file)
         formData.append("file", file);
 
+        setFetchingTranscript(true);
         fetch(API_ENDPOINT, {
             method: "post",
             body: formData,
         })
-            .then((res) => console.log(res))
-            .catch((err) => ("Error occurred", err));
+            .then((response) => response.json())
+            .then((res) => {
+                setTranscript(res.transcript);
+                setFetchingTranscript(false);
+            })
+            .catch((err) => {
+                console.log(err);
+                setFetchingTranscript(false);
+            });
     };
 
     const startRecord = () => {
@@ -61,18 +78,28 @@ const DeepgramHandler = (props) => {
             )}
 
             {props.children}
+
+            {fetchingTranscript && <p>fetching transcript...</p>}
+            {transcript && <p>{transcript}</p>}
         </>
     );
 };
 
 function App() {
+    const [value, setValue] = useState("");
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    <DeepgramHandler>
-                        <input style={{ padding: 15 }} />
+                    <DeepgramHandler setValue={setValue}>
+                        <input
+                            value={value}
+                            onChange={(ev) => {
+                                setValue(ev.target.value);
+                            }}
+                            style={{ padding: 15 }}
+                        />
                     </DeepgramHandler>
                 </p>
             </header>
